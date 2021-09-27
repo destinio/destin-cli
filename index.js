f1d8e8522fbba6e72d1466330269a2fae81403f5
Destin Lee <dleeinc@gmail.com>
2021-09-27 15:10:12 -0500

    info

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
 
-console.log('Destin Lee 2');
+console.log(`
+Hello, I'm Destin 👋
+
+I'm a Software Developer. Talent Manager. Code Whisperer
+
+In my free time I:
+
+👨‍👩‍👦‍👦 Hang with the familee
+🎲 Play board games
+🍝 Cook amazing meals
+📖 Read
+☯️ Practice Zen
+
+🌱 I’m currently learning
+
+- Rails - https://github.com/destinio/rails-demo
+
+🔭 I’m currently working on
+
+- Learning the ways of the Fullstack Dev
+
+⚡️ Fun facts
+
+Currenly reading through the extended Dark Tower series (currently on Wolves of the Calla) 🐺
+Father of two 👨‍👩‍👦‍👦
+I can solve a Rubix Cube in under a minute
+Kicked cancer's ass and have been cancer free for two years 🩸
+`);
