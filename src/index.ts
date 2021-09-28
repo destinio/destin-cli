fbd6d5e84ac89094861e12d88739e62d72eed196
Destin Lee <dleeinc@gmail.com>
2021-09-28 13:52:33 -0500

    ts working v3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-#!/usr/bin/env node
-import { welcome } from './apps/welcome.js';
 import { clear } from './utils/clear.js';
 import { log } from './utils/log.js';
 
+// WELCOME APP
+import { welcome } from './apps/welcome.js';
 welcome();
 
 // EXPORTS
