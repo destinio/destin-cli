eed5dbcb48b402d945c40591251e625e305225da
Destin Lee <dleeinc@gmail.com>
2021-11-08 12:01:09 -0600

    libs

--- TODO.md
+++ TODO.md
@@ -3,3 +3,18 @@ lerna
 fs-extra
 commander
 prompts
+
+## Tools of the CLI Trade
+
+- inquirer- for complex input prompts
+- commander
+- meow or arg - basic argument parsing
+
+- email-prompt - convenient email input prompts
+- chalk - colored output
+- ora - beautiful spinners
+- boxen - drawing boxes around your output
+- listr - progress lists
+- ink - build CLIs with React
+- commander and yargs - complex argument parsing and subcommand support
+- oclif a framework - building extensible CLIs by Heroku (gluegun as an alternative)
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,26 @@
 
 // WELCOME APP
 import { welcome } from './apps/welcome';
+import { Command } from 'commander';
+import { clear } from './utils/clear';
+import chalk = require('chalk');
+import { header } from './utils/semantics';
 
-welcome();
+const program = new Command();
+
+program.version('0.1.0');
+
+program.option('-t, --ts', 'create tsconfig thingers');
+
+program.parse(process.argv);
+
+if (!process.argv[2]) {
+  welcome();
+} else {
+  const { ts } = program.opts();
+
+  if (ts) {
+    clear();
+    header(` Welcome to the TS app `);
+  }
+}
