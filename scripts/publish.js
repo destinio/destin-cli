b20775914fae9bc861dffffbdf33a496d12b1cd1
Destin <dleeinc@gmail.com>
2021-11-19 17:10:30 -0600

    versionbump

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,11 +3,18 @@
 import { loadJsonFile } from 'load-json-file'
 import inquirer from 'inquirer'
 import chalk from 'chalk'
-import child_process from 'child_process'
+import { exec } from 'child_process'
 import gradient from 'gradient-string'
 import ora from 'ora'
+
+function clear() {
+  process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
+}
+
 ;(async () => {
   const preVersion = await loadJsonFile(`${process.cwd()}/package.json`).version
+
+  clear()
 
   const { bump } = await inquirer.prompt([
     {
@@ -19,36 +26,22 @@ import ora from 'ora'
     },
   ])
 
-  child_process.exec(`npm version ${bump}`, (error, stdout, _stderr) => {
-    const publishSpinner = ora(`${gradient.rainbow('Publishing')}`).start()
-    publishSpinner.spinner = 'fingerDance'
-
+  exec(`npm version ${bump}`, (error, stdout, _stderr) => {
     if (error) {
       console.log(chalk.redBright.bold('Git working directory not clean.'))
       return
     }
 
-    child_process.exec('npm publish', (error, stdout, _stderr) => {
+    const publishSpinner = ora(`${gradient.rainbow('Publishing')}`).start()
+    publishSpinner.spinner = 'fingerDance'
+
+    exec('npm publish', (error, stdout, _stderr) => {
       if (error) {
         console.log(`${chalk.redBright.bold('🤔 hummm something went wrong')}`)
         return
       }
       publishSpinner.text = `${stdout} has been published`
       publishSpinner.succeed()
-      // console.log(chalk.greenBright.bold(`${stdout} was published 🚀`))
     })
   })
-
-  // switch (bump) {
-  //   case 'minor':
-  //     console.log('minor')
-  //     child_process.exec('npm version minor')
-  //     break
-  //   case 'major':
-  //     console.log('major')
-  //     break
-  //   default:
-  //     console.log('patch')
-  //     break
-  // }
 })()
